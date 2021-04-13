8c72a9d2cad673574729580275d8519075651d23
Natalie <natalie.ramer@ou.edu>
2021-04-13 16:12:40 -0500

    * OklahomaLottery.csproj: Clean * WeatherForecastController.cs: Clean
    * Program.cs: Code update

--- OklahomaLottery/Program.cs
+++ OklahomaLottery/Program.cs
@@ -1,26 +1,79 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
-namespace OklahomaLottery
+namespace OklahomaLottery 
 {
-    public class Program
+    [DataContract]
+    internal class Plays
     {
-        public static void Main(string[] args)
+        [DataMember(Name = "date_played")]
+        public int Date_played { get; set; }
+        [DataMember(Name = "games_played")]
+        public string[] Games_played { get; set; }
+    }
+
+    internal class Program
+    {
+
+
+        private const string Url = "https://www.lottery.ok.gov/plays.json";
+
+        private static void Main()
         {
-            CreateHostBuilder(args).Build().Run();
+
+
+            GetPlaysAsync();
+            Console.ReadLine();
+
+
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+
+        private static async void GetPlaysAsync()
+        {
+            string Game_1;
+            Console.WriteLine($"What is the first game?");
+            Game_1 = Console.ReadLine();
+
+            int count = 0;
+            int i = 0;
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetStringAsync(Url);
+
+            var plays = JsonConvert.DeserializeObject<Plays[]>(response);
+
+            foreach (var game in plays)
+            {
+
+                int date_played = game.Date_played;
+                string[] games_played = game.Games_played;
+                if (games_played[i] == Game_1)
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+
+                    count++;
+                    i++;
+
+                }
+                else
+                {
+                    i++;
+
+                }
+
+            }
+
+            Console.WriteLine(Game_1 + "is played" + count + "times");
+            Console.ReadLine();
+
+
+
+
+        }
+
     }
+
 }
